742529e310b2a3e4b1d7acbf515ff03c823bb6ac
Labib361 <nadiamahbuba.webdevsoln@gmail.com>
2025-07-05 18:39:57 +0600

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Profile from '../../images/profile.png'
+const Header = () => {
+    return (
+        <div className='border-b-2 border-gray-400 flex justify-between max-w-screen-xl mx-auto p-[20px]'>
+            <h1 className='font-bold text-[40px]'>Knowledge Cafe</h1>
+            <img src={Profile} alt="" className='border-black rounded-full border-2' />
+        </div>
+    );
+};
+
+export default Header;
